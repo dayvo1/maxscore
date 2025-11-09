8f3c3fe732976e7b97e7619eb554bff15f53faab
David Gonzalez <dgonzalez@chapman.edu>
2025-11-08 17:27:33 -0800

    updated maxscore.sh

--- maxscore.sh
+++ maxscore.sh
@@ -13,9 +13,9 @@ arr[0]=$max
 # loop 4 more times and ask user for num input and store inside array
 for i in {1..4}; do
     read -r num
-    arr[$i]=$num
+    arr[i]=$num
 
-    if (($num > $max)); then
+    if ((num > max)); then
         max=$num
     fi
 done
@@ -24,7 +24,7 @@ echo "The highest score is: $max"
 echo "The scores are: "
 # while index <= 5 print out score and the diff between max and score
 for i in {0..4}; do
-    echo "${arr[$i]} differs from max by $(($max - ${arr[$i]}))"
+    echo "${arr[$i]} differs from max by $((max - ${arr[$i]}))"
 done
 
 
